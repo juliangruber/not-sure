f577984d2fa20848f6aa716b2dafbc16271dac86
Julian Gruber <julian@juliangruber.com>
2012-06-20 11:22:38 +0200

    initial commit

---
+++ not-sure.js
@@ -0,0 +1,15 @@
+;(function() {
+	var fs = require('fs')
+	  , exec = require('child_process').exec
+	  , stream = require('stream');
+	
+	fs.readdir('.', function(err, files) {
+		if (files.indexOf('.git') !== -1) {
+			exec('ls', function(err, stdin, stdout) {
+				console.log(stdin);
+			});
+		};
+	})
+	
+	
+})();
