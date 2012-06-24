e7e191ebc1508e1a6885ac0b6500f20e82e69e25
Julian Gruber <julian@juliangruber.com>
2012-06-24 11:04:47 +0200

    more documentation, refactorings and better output

--- not-sure.js
+++ not-sure.js
@@ -2,27 +2,35 @@
 	
 	var fs = require('fs');
 	var exec = require('child_process').exec;
-	var stream = require('stream');
 	var rl = require('readline');
-	var i = rl.createInterface(process.stdin, process.stdout, null);
-	var diff = '';
 
 	/**
 	 * Shellscript that posts diffs to for-sure CodeReview server
-	 * @type Object
 	 */
 	var notSure = {};
 	
-	
-	notSure.getDiff = function(scm, cb) {
-		exec(scm+' diff', function(err, stdin, stdout) {
+	/**
+	 * Execute a git/svn diff and pass results to `cb()`
+	 * 
+	 * @param {String} cmd VCS shell command
+	 * @param {Function} cb Gets passed errors and diff output
+	 */
+	notSure.getDiff = function(cmd, cb) {
+		exec(cmd+' diff', function(err, stdin, stdout) {
 			if (err) return cb(err);
 			if (stdin.length === 0) return cb(new Error('No files changes'));
 			cb(null, stdin);
 		});
 	};
-		
-	notSure.chooseScm = function(path, cb) {
+	
+	/**
+	 * Check the filesystem for a `.git`/`.svn` folder in `path`
+	 * and pass the first found VCS to `cb()`
+	 *
+	 * @param {String} path Working directory
+	 * @param {Function} cb Gets passed errors and Vcs name
+	 */
+	notSure.findVcs = function(path, cb) {
 		fs.readdir(path, function(err, files) {
 			if (err) return cb(err);
 			if (files.indexOf('.git') > -1) return cb(null, 'git');
@@ -31,7 +39,13 @@
 		});
 	};
 	
+	/**
+	 * Ask for `title` and `description` of the issue and developers to ask
+	 *
+	 * @param {Function} cb Gets passed answers object
+	 */
 	notSure.askQuestions = function(cb) {
+		var i = rl.createInterface(process.stdin, process.stdout, null);
 		i.question('Title of issue: ', function(title) {
 			i.question('Description: ', function(description) {
 				i.question('Developers to ask: ', function(developers) {
@@ -49,6 +63,13 @@
 		})
 	};
 	
+	/**
+	 * Helper function for underlining a string
+	 *
+	 * @param {String} string
+	 * @param {String} char Character to underline `string` with
+	 * @returns {String} Underlined string
+	 */
 	notSure.underline = function(string, char) {
 		char = char || '-';
 		var underline = '';
@@ -56,27 +77,34 @@
 		return string+'\n'+underline;
 	}
 	
+	/**
+	 * Perform the whole dance from checks and questions to the finished
+	 * output
+	 */
 	notSure.ask = function() {
-		notSure.chooseScm('.', function(err, scm) {
+		notSure.findVcs('.', function(err, scm) {
 			if (err) throw err;
-			notSure.getDiff(scm, function(err, _diff) {
-				if (err) throw err;
-				diff = _diff;
-			});
-		});
+			notSure.getDiff(scm, function(err, diff) {
+				if (err) throw err;				
+				notSure.askQuestions(function(err, answers) {
+					if (err) throw err;
 
-		notSure.askQuestions(function(err, answers) {
-			if (err) throw err;
-			
-			process.stdout.write('\u001B[2J\u001B[0;0f');
-			
-			console.log(notSure.underline(answers.title, '='));
-			console.log(answers.description+'\n');
-			console.log('asked: '+answers.developers+'\n');
-			console.log(diff);
+					// clear the terminal
+					for (var i=0;i<5;i++) console.log('');
+					console.log('\u001B[2J\u001B[0;0f');
+
+					console.log(notSure.underline(answers.title, '='));
+					console.log(answers.description+'\n');
+					console.log('asked: '+answers.developers+'\n');
+					console.log(diff);
+				});
+			});
 		});
 	};
 	
+	/**
+	 * Only call `ask()` if not included by another module.
+	 */
 	if (!module.parent) {
 		notSure.ask()
 	} else {
